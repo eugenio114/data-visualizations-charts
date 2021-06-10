16acebe255b091a89e1a60f5151b772b9b58eff1
Eugenio <76439879+eugenio114@users.noreply.github.com>
2021-06-10 14:03:39 +0200

    Add files via upload

---
+++ matplotlib/Histogram Matplotlib.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import csv
+import pandas as pd
+
+fifa = pd.read_csv('./data_csv/data_fifa.csv')
+print(fifa.head(5))
+
+###HISTOGRAM
+bins = [40,50,60,70,80,90,100]
+
+plt.hist(fifa.Overall, bins=bins, color='#abcdef')
+plt.xticks(bins)
+
+plt.ylabel('Number of players')
+plt.xlabel('Skill level')
+plt.title ('Distribution of Player Skills in Fifa 2018')
+
+#plt.yticks([0, 100])
+
+plt.show()
+plt.savefig('Histogram - Distribution of Players Skill Level Fifa 2018', dpi=300)
